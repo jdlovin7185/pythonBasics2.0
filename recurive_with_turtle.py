a8b1fb4c115c8c29866b570e69da449883c0c5a4
Joshua Lovin <jdlovin7185@gmail.com>
2023-01-03 19:58:35 -0500

    tower of hanoi & turtles

--- recurive_with_turtle.py
+++ recurive_with_turtle.py
@@ -1,8 +1,9 @@
 import turtle
 
 my_turtle = turtle.Turtle()
+second_turtle = turtle.Turtle()
 my_win = turtle.Screen()
-my_win.setup(500, 500)
+my_win.setup(600, 600)
 
 
 def draw_spiral(slow_poke, line_len):
@@ -10,8 +11,18 @@ def draw_spiral(slow_poke, line_len):
         return
     else:
         slow_poke.forward(line_len)
-        slow_poke.left(90)
+        slow_poke.left(120)
         draw_spiral(slow_poke, line_len - 10)
 
 
-draw_spiral(my_turtle, 150)
+def draw_backward_spiral(slow_poke, line_len):
+    if line_len <= 0:
+        return
+    else:
+        slow_poke.backward(line_len)
+        slow_poke.right(120)
+        draw_spiral(slow_poke, line_len - 10)
+
+
+draw_backward_spiral(second_turtle, 200)
+draw_spiral(my_turtle, 200)
